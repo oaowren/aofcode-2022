bba9b6b98fc232d2b43549da25b2e860c839dca9
Odd André Owren <oddandreowren@gmail.com>
2022-12-12 21:17:18 +0100

    7th and 12th

--- 12des.scala
+++ 12des.scala
@@ -1,16 +1,75 @@
 import scala.io.Source
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ListBuffer, Queue}
 
-object TempDec {
-  val day = 1
+object TwelfthDec {
+  val day = 12
 
-  def readFile(filename: String, cb: String => Unit): Unit = {
-    for (line <- Source.fromFile(filename).getLines) {
-      cb(line)
+  var map = ListBuffer[String]()
+  var values = ListBuffer[ListBuffer[Int]]()
+
+  var myPos: (Int, Int) = (0,0)
+  var goalPos: (Int, Int) = (0,0)
+
+  var possibleEnds: ListBuffer[(Int, Int)] = ListBuffer()
+
+  var visited: ListBuffer[(Int, Int)] = ListBuffer()
+  var toVisit: Queue[(Int, Int)] = Queue()
+
+  def readFile(filename: String): Unit = {
+    for ((line, index) <- Source.fromFile(filename).getLines.zipWithIndex) {
+      map += line
+      values += ListBuffer(line.toCharArray().map(_ => Int.MaxValue)*)
+      if (line.contains("S")){
+        myPos = (index, line.indexOf("S"))
+      }
+      for ((l, i) <- line.toCharArray().zipWithIndex){ if (l == 'a' || l == 'S') possibleEnds += ((index, i)) else ()}
+      if (line.contains("E")){
+        goalPos = (index, line.indexOf("E"))
+        values(goalPos._1)(goalPos._2) = 0
+        toVisit.enqueue(goalPos)
+      }
     }
   }
 
+  def allowedMove(pos: (Int, Int), nextPos: (Int, Int)): Boolean = {
+    var c = map(pos._1).charAt(pos._2)
+    var nextC = map(nextPos._1).charAt(nextPos._2)
+    if (c == 'E'){
+        c = 'z'
+    } else if (nextC == 'S') {
+        nextC = 'a'
+    }
+    c.toInt - nextC.toInt <= 1
+  }
+
+  def isDefined(pos: (Int, Int)): Boolean = {
+    pos._1 >= 0 && pos._2 >= 0 && pos._1 < map.length && pos._2 < map.head.length()
+  }
+
+  def getPossibleMoves(currentPos: (Int, Int)): Unit= {
+    val moveU = (currentPos._1 - 1, currentPos._2)
+    val moveD = (currentPos._1 + 1, currentPos._2)
+    val moveL = (currentPos._1, currentPos._2 - 1)
+    val moveR = (currentPos._1, currentPos._2 + 1)
+    ListBuffer(moveU, moveD, moveL, moveR).filter(m => isDefined(m) && allowedMove(currentPos, m) && !toVisit.exists(p=> m == p) && !visited.exists(p => m == p)).foreach(m => {
+          values(m._1)(m._2) = Math.min(values(m._1)(m._2), values(currentPos._1)(currentPos._2) + 1)
+          toVisit.enqueue(m)
+        })
+  }
+
+  def allPossibleEndsFound(possibleEnds: ListBuffer[(Int, Int)]): Boolean = {
+    !possibleEnds.exists(e => values(e._1)(e._2) == Int.MaxValue)
+  }
+
   def main(args: Array[String]) = {
-    readFile(s"inputs_2022/day_${day}_small.txt")
+    readFile(s"inputs_2022/day_${day}_large.txt")
+    // If queue is empty, we have visited all possible ends
+    while (!allPossibleEndsFound(possibleEnds) && !toVisit.isEmpty){
+        var next = toVisit.dequeue()
+        getPossibleMoves((next._1, next._2))
+        visited += ((next._1, next._2))
+    }
+    println(values(myPos._1)(myPos._2))
+    println(possibleEnds.map(e => values(e._1)(e._2)).min)
   }
 }
--- 7des.scala
+++ 7des.scala
@@ -1,16 +1,84 @@
 import scala.io.Source
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{ListBuffer, Map}
+
+case class File(parent: Dir, name: String, size: Long)
+case class Dir(parent: Option[Dir], name: String) {
+  val files: ListBuffer[File] = ListBuffer()
+  val dirs: Map[String, Dir] = Map.empty
+  
+  def size: Long = files.map(_.size).sum + dirs.values.map(_.size).sum
+  
+  def addFile(f: File): Unit = files += f
+  def addDir(d: Dir): Option[Dir] = dirs.put(d.name, d)
+  
+  def getDir(name: String): Option[Dir] = dirs.get(name)
+}
 
 object SeventhDec {
   val day = 7
+
+  val cdPattern = "\\$ cd ([a-z]+)".r
+  val dirPattern = "dir ([a-z]+)".r
+  val filePattern = "([0-9]+) (.+)".r
+
+  val root = Dir(None, "/")
+  var cwd = root
+
+  def parseTerminalInput(line: String): Unit = {
+    line match {
+      case "$ cd /" => cwd = root
+      case "$ cd .." => cwd = cwd.parent.getOrElse(cwd)
+      case cdPattern(dirName) => {
+        cwd.getDir(dirName) match {
+          case Some(dir) => cwd = dir
+          case None => {
+            val newDir = Dir(Some(cwd), dirName)
+            cwd.addDir(newDir)
+            cwd = newDir
+          }
+        }
+      }
+      case "$ ls" =>
+      case dirPattern(dirName) => cwd.getDir(dirName) match {
+        case None => cwd.addDir(Dir(Some(cwd), dirName))
+        case Some(_) =>
+      }
+      case filePattern(size, fileName) => cwd.addFile(File(cwd, fileName, size.toLong))
+    }
+  }
+
+  def dirSums(dirs: List[Dir], maxSizeToCount: Long): Long = {
+      dirs.foldLeft(0L) { (acc, dir) =>
+        val dirSize = dir.size match {
+          case s if s <= maxSizeToCount => s
+          case _ => 0
+        }
+        
+        val subdirsSize = dirSums(dir.dirs.values.toList, maxSizeToCount)
+        
+        acc + dirSize + subdirsSize
+      }
+    }
 
   def readFile(filename: String, cb: String => Unit): Unit = {
     for (line <- Source.fromFile(filename).getLines) {
       cb(line)
     }
   }
+  
+  def dirSizes(dirs: List[Dir]): List[Long] = {
+      dirs.foldLeft[List[Long]](Nil) { (acc, dir) =>
+        (dir.size :: acc) ::: dirSizes(dir.dirs.values.toList)
+      }
+    }
 
   def main(args: Array[String]) = {
-    readFile(s"inputs_2022/day_${day}_small.txt")
+    readFile(s"inputs_2022/day_${day}_large.txt", parseTerminalInput)
+    println(dirSums(List(root), 100000L))
+    val additionalSpaceNeeded = 30000000 - (70000000 - root.size)
+    println(dirSizes(List(root)).sorted.collectFirst {
+      case size if size > additionalSpaceNeeded => size
+    })
+    
   }
 }
